903091b2e2e0c1a16eda2e8fe708d6b127f69536
CARLOS EDUARDO CAMARGO DE SIQUEIRA <116232984+Caarloseduardocs@users.noreply.github.com>
2022-12-21 00:11:14 -0300

    Add files via upload

---
+++ Cap4-Dicionarios_de_dados/Sistema_com_dicionário.py
@@ -0,0 +1,12 @@
+from
+
+usuarios={}
+opcao= perguntar ()
+while opcao=="I" or opcao=="P" or opcao=="E" or opcao=="L":
+    if opcao=="I":
+        chave=input("Digite o login: ").upper()
+        nome=input("Digite o nome: ").upper()
+        data=input("Digite a última data de acesso: ")
+        estacao=input("Qual a última estação acessada: ").upper()
+        usuarios[chave]=[nome, data, estacao]
+    opcao = perguntar ()
